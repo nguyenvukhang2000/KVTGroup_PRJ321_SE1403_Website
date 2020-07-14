8ba24751a10568cac4e72b968de7f0b29b3b5ae7
nguyenvukhang2000 <nguyenvukhang01@gmail.com>
2020-07-14 20:12:10 +0700

    reload file lại vì lỗi

---
+++ KVTGroup/src/java/Controller/User/SearchForProduct.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.User;
+
+import Models.DAO.ProductsDAO;
+import Models.Entities.Product;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author KhangNVCE140224
+ */
+public class SearchForProduct extends HttpServlet {
+    ArrayList<Product> products = new ArrayList<>();
+    ProductsDAO pDAO = new ProductsDAO();
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SearchForProduct</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SearchForProduct at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getParameter("search") != null) {
+            String productName  = request.getParameter("search");
+            
+            System.out.println(productName);
+            
+            products = pDAO.getProductByName(productName);
+            System.out.println(products.size());
+        } else {
+            System.out.println("else");
+            int min = Integer.parseInt(request.getParameter("down"));
+            System.out.println(min);
+            int max = Integer.parseInt(request.getParameter("up"));
+            System.out.println(max);
+            
+            products = pDAO.getAllProductByPrice(min, max);
+            System.out.println("sizeof product" + products.size());
+        }
+        
+        request.setAttribute("allProducts", products);
+        
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/shop.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
